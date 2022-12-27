cee31d8d3dc3462c47159f716b2a2c6cd7cb3c37
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-27 20:16:58 +0700

    feat: init fastify

---
+++ index.ts
@@ -0,0 +1,24 @@
+import fastify from "fastify";
+import fetch from "node-fetch";
+
+const app = fastify({ logger: true });
+
+// Declare a route
+app.get("/", async (_, reply) => {
+  reply.header("Content-Type", "image/svg+xml");
+
+  const response = await fetch(
+    "https://img.shields.io/badge/swc-111111?style=for-the-badge&logo=swc&logoColor=F5D140"
+  );
+  const textResponse = await response.text();
+
+  reply.send(textResponse);
+});
+
+// Run the server!
+app.listen({ port: 3000 }, (err) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
