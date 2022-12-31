31959c533f9da7b04b6212bef146d8356bfa9240
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-31 17:11:45 +0700

    feat: add tools

---
+++ api/badges/tools.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: "Commitlint",
+    url: "https://img.shields.io/badge/commitlint-121212?style=for-the-badge&logo=commitlint&logoColor=51b1dd",
+  },
+  {
+    name: "ESLint",
+    url: "https://img.shields.io/badge/ESLint-4B32C3?style=for-the-badge&logo=eslint",
+  },
+  {
+    name: "Prettier",
+    url: "https://img.shields.io/badge/prettier-1B2B34?style=for-the-badge&logo=prettier",
+  },
+];
