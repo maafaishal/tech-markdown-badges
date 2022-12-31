87a14a48d122c256a38913f96dc4764aa1a11a17
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-31 17:12:11 +0700

    feat: add bundlers

--- api/badges/bundlers.ts
+++ api/badges/bundlers.ts
@@ -1,10 +1,18 @@
 export default [
   {
-    name: "babel",
+    name: "Babel",
     url: "https://img.shields.io/badge/Babel-F9DC3e?style=for-the-badge&logo=babel&logoColor=black",
   },
   {
-    name: "swc",
+    name: "Esbuild",
+    url: "https://img.shields.io/badge/esbuild-191919?style=for-the-badge&logo=esbuild",
+  },
+  {
+    name: "SWC",
     url: "https://img.shields.io/badge/swc-111111?style=for-the-badge&logo=swc&logoColor=F5D140",
+  },
+  {
+    name: "Vite",
+    url: "https://img.shields.io/badge/vite-646CFF?style=for-the-badge&logo=vite&logoColor=white",
   },
 ];
