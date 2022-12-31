1e20ab3b278c588806eaf83deb186751af71a15d
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-31 17:18:32 +0700

    refactor: export all badges

--- api/badges/index.ts
+++ api/badges/index.ts
@@ -1,3 +1,8 @@
 import bundlers from "./bundlers"
+import databases from "./databases"
+import frameworks from "./frameworks&libraries"
+import languages from "./languages"
+import tools from "./tools"
+import uiLibraries from "./uiLibraries"
 
-export default [...bundlers]
+export default [...bundlers, ...databases, ...frameworks,...languages, ...tools, ...uiLibraries ]
