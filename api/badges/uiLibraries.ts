cb3ce236c57a0b887725ac3310932ef6fecd405d
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-31 17:11:39 +0700

    feat: add ui libraries

---
+++ api/badges/uiLibraries.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    name: "Ant-Design",
+    url: "https://img.shields.io/badge/-AntDesign-0170FE?style=for-the-badge&logo=ant-design&logoColor=white",
+  },
+  {
+    name: "Bootstrap",
+    url: "https://img.shields.io/badge/bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white",
+  },
+  {
+    name: "Bulma",
+    url: "https://img.shields.io/badge/bulma-00D0B1?style=for-the-badge&logo=bulma&logoColor=white",
+  },
+  {
+    name: "Chakra",
+    url: "https://img.shields.io/badge/chakra-2BBFB3?style=for-the-badge&logo=chakraui&logoColor=white",
+  },
+  {
+    name: "MUI",
+    url: "https://img.shields.io/badge/MUI-0081CB?style=for-the-badge&logo=mui&logoColor=white",
+  },
+  {
+    name: "Semantic UI",
+    url: "https://img.shields.io/badge/Semantic%20ui-35BCB2?style=for-the-badge&logo=SemanticUIReact&logoColor=white",
+  },
+  {
+    name: "Styled Components",
+    url: "https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white",
+  },
+  {
+    name: "TailwindCSS",
+    url: "https://img.shields.io/badge/tailwindcss-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+  },
+  {
+    name: "Windicss",
+    url: "https://img.shields.io/badge/windicss-48B0F1?style=for-the-badge&logo=windi-css&logoColor=white",
+  },
+];
