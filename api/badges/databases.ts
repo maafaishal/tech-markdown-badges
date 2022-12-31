9952fa65e2054a7a82861b42602fc512fca330f3
Muhammad A Faishal <maafaishal@gmail.com>
2022-12-31 17:12:05 +0700

    feat: databases

---
+++ api/badges/databases.ts
@@ -0,0 +1,82 @@
+export default [
+  {
+    name: "AmazonDynamoDB",
+    url: "https://img.shields.io/badge/Amazon%20DynamoDB-4053D6?style=for-the-badge&logo=Amazon%20DynamoDB&logoColor=white",
+  },
+  {
+    name: "ApacheCassandra",
+    url: "https://img.shields.io/badge/cassandra-1287B1?style=for-the-badge&logo=apache-cassandra&logoColor=white",
+  },
+  {
+    name: "CockroachLabs",
+    url: "https://img.shields.io/badge/Cockroach%20Labs-6933FF?style=for-the-badge&logo=Cockroach%20Labs&logoColor=white",
+  },
+  {
+    name: "Couchbase",
+    url: "https://img.shields.io/badge/Couchbase-EA2328?style=for-the-badge&logo=couchbase&logoColor=white",
+  },
+  {
+    name: "Firebase",
+    url: "https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=Firebase&logoColor=white",
+  },
+  {
+    name: "InfluxDB",
+    url: "https://img.shields.io/badge/InfluxDB-22ADF6?style=for-the-badge&logo=InfluxDB&logoColor=white",
+  },
+  {
+    name: "MariaDB",
+    url: "https://img.shields.io/badge/MariaDB-003545?style=for-the-badge&logo=mariadb&logoColor=white",
+  },
+  {
+    name: "MusicBrainz",
+    url: "https://img.shields.io/badge/Musicbrainz-EB743B?style=for-the-badge&logo=musicbrainz&logoColor=BA478F",
+  },
+  {
+    name: "MicrosoftSQLServer",
+    url: "https://img.shields.io/badge/Microsoft%20SQL%20Sever-CC2927?style=for-the-badge&logo=microsoft%20sql%20server&amte",
+  },
+  {
+    name: "MongoDB",
+    url: "https://img.shields.io/badge/MongoDB-4ea94b?style=for-the-badge&logo=mongodb&logoColor=white",
+  },
+  {
+    name: "MySQL",
+    url: "https://img.shields.io/badge/mysql-00f?style=for-the-badge&logo=mysql&logoColor=white",
+  },
+  {
+    name: "Neo4J",
+    url: "https://img.shields.io/badge/Neo4j-008CC1?style=for-the-badge&logo=neo4j&logoColor=white",
+  },
+  {
+    name: "Postgres",
+    url: "https://img.shields.io/badge/postgres-316192?style=for-the-badge&logo=postgresql&logoColor=white",
+  },
+  {
+    name: "Realm",
+    url: "https://img.shields.io/badge/Realm-39477F?style=for-the-badge&logo=realm&logoColor=white",
+  },
+  {
+    name: "Redis",
+    url: "https://img.shields.io/badge/redis-DD0031?style=for-the-badge&logo=redis&logoColor=white",
+  },
+  {
+    name: "Single Store",
+    url: "https://img.shields.io/badge/Single%20Store-AA00FF?style=for-the-badge&logo=singlestore&logoColor=white",
+  },
+  {
+    name: "SQLite",
+    url: "https://img.shields.io/badge/sqlite-07405e?style=for-the-badge&logo=sqlite&logoColor=white",
+  },
+  {
+    name: "Supabase",
+    url: "https://img.shields.io/badge/Supabase-3ECF8E?style=for-the-badge&logo=supabase&logoColor=white",
+  },
+  {
+    name: "SurrealDB",
+    url: "https://img.shields.io/badge/SurrealDB-FF00A0?style=for-the-badge&logo=surrealdb&logoColor=white",
+  },
+  {
+    name: "Teradata",
+    url: "https://img.shields.io/badge/Teradata-F37440?style=for-the-badge&logo=teradata&logoColor=white",
+  },
+];
